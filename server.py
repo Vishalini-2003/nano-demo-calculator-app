dcbc86aabed39b1864ae01eafcbe68ffe01add24
vishalini <vishalini@gmail.com>
2023-09-10 15:05:27 +0530

    sixthcommit

--- server.py
+++ server.py
@@ -1,19 +1,25 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello World!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    arr=request.json
+    var1=arr['first']
+    var2=arr['second']
+    return {'result':var1+var2}
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    arr=request.json
+    var1=arr['first']
+    var2=arr['second']
+    return {'result':var1-var2}
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
